95d1e0fdd8e96b17b30550f25cf44c4bffd00b5c
Saanvi <163568541+1118540@users.noreply.github.com>
2025-09-12 16:46:38 +0000

    I am mastering Print line !!

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,24 @@
-// Student name
-// class period
-// project name
-// date
+// Saanvi Shah
+// P2
+// Zero Hero
+// 9/12/25
 
 /*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
+ * DESCRIPTION: Whatever Mr. Reiber says
+ * INPUT: We don't know yet :(
+ * OUTPUT: Some cool lines in the command terminal thing
+ * EDGE CASE: Every case is an edge case
  */
 
 package edu.bhscs;
+
+public class Main {
+  public static void main(String[] args){
+    System.out.println("-------------------");
+    System.out.println("!!!!!! PROGRAM START !!!!!");
+    System.out.println("hello");
+    System.out.println("!!!!!! PROGRAM END !!!!!");
+    System.out.println("-------------------");
+  }
+
+}
