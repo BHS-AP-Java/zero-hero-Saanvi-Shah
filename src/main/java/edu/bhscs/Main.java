065748268ccd8da1995413e7099411a4f7660879
Saanvi <163568541+1118540@users.noreply.github.com>
2025-09-15 16:48:39 +0000

    I am learning about message to commit, very helpful

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,23 +1,36 @@
 // Saanvi Shah
 // P2
-// Zero Hero
-// 9/12/25
+// Variables: Replacement is the gateway to abstractions
+// 9/15/25
 
 /*
- * DESCRIPTION: Whatever Mr. Reiber says
+ * DESCRIPTION: This excercise is about learning about variables, constants, and types
  * INPUT: We don't know yet :(
  * OUTPUT: Some cool lines in the command terminal thing
  * EDGE CASE: Every case is an edge case
  */
 
 package edu.bhscs;
+class Main{
 
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("-------------------");
-    System.out.println("!!!!!! PROGRAM START !!!!!");
-    System.out.println("hello");
-    System.out.println("!!!!!! PROGRAM END !!!!!");
-    System.out.println("-------------------");
+  // this is an example of a 'char' constant
+  // because it is global, it should be in capital letters
+  public static final char A = 'a';
+
+  public static void main(String[] args){
+
+    int someNumber = 67;
+
+    System.out.println("67!" + A + someNumber);
+    System.out.print(A + "?" + A + someNumber);
+    System.out.print(A + A + someNumber);
+    System.out.println();
+    System.out.println("this is neet");
+    System.out.println();
+    System.out.println();
+    System.out.println("OK I am done");
+    
+
   }
+
 }
