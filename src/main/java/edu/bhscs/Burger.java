95d52e7350ad2d0025124e225cb312d966f9e6e6
Saanvi <163568541+1118540@users.noreply.github.com>
2025-10-11 23:27:30 +0000

    Added burger with loop

--- src/main/java/edu/bhscs/Burger.java
+++ src/main/java/edu/bhscs/Burger.java
@@ -1,60 +1,63 @@
+// Saanvi Shah
+// 10/11/25
+// P2
+// This is making the burger class!!!
+
+/*
+ * DESCRIPTION: The Burger class represents the burger that the restaurant makes and sells. It holds
+ * details about size, bun type, weight, and cheese. It can print an ASCII burger using a loop for
+ * the patty layers.
+ *
+ * INPUT: size, bunType, burgerWeight, cheese
+ *
+ * OUTPUT: A visual ASCII burger and descriptions of the burger.
+ *
+ * EDGE CASES: - Different burger sizes - With or without cheese
+ */
+
 package edu.bhscs;
 
 public class Burger {
-  //fields and properties
+  // fields and properties
   String size;
   String bunType;
   int burgerWeight;
   boolean cheese;
 
-  //contructors
+  // constructors
   public Burger(String size, String bunType, int burgerWeight, boolean cheese) {
     this.size = size;
     this.bunType = bunType;
     this.cheese = cheese;
     this.burgerWeight = burgerWeight;
-
   }
 
-  //methods
+  // methods
   public void showBurger() {
     System.out.println("\nHere is your burger:");
+  }
 
-
-}
-  public void beEaten()
-  {
+  public void beEaten() {
     System.out.println("Nom nom nom! The burger is delicious!");
   }
 
-  public void drawBurger()
-  {
-String s = "            \r\n" +
- "                     \r\n" +
- "      _..----.._       \r\n" +
- "    .'     o    '.      \r\n" +
- "   /   o       o  \\    \r\n" +
-  "  |o        o     o|   \r\n" +
- "  /'-.._o     __.-'\\   \r\n" +
- "  \\      `````     /  \r\n" +
-  "  |``--........--'`|    \r\n" +
-  "  \\               /                \r\n" +
-  "     `'----------'`                   \r\n" +
-  "---------------------------------------\r\n" +
- "     .-/-.                       \r\n" +
- "   | -'|                      \r\n" +
-    "  |   |                      \r\n" +
-     " |   |   .-\"\"\"\"-.           \r\n" +
-      "\\___/  /' .  '. \\   \\|/\\// \r\n" +
-       "     (`-..:...-')  |`\"\"`| \r\n" +
-        "     ;-......-;   |    | \r\n" +
-         "     '------'    \\____/ \r\n" +
-"------------------------------------------------ \r\n";
-    System.out.println(s);
+  public void drawBurger() {
+    // Top bun
+    System.out.println("   ----");
+    System.out.println("  /    \\");
 
+    // Patty layers using a loop - minimal keystrokes version
+    for (int i = 0; i < burgerWeight; i++) {
+      System.out.println(" |~~~~~~|");
+    }
+
+    // Cheese layer if applicable
+    if (cheese) {
+      System.out.println(" |[====]|");
+    }
+
+    // Bottom bun
+    System.out.println("  \\    /");
+    System.out.println("   ----");
   }
-  }
-
-
-
-
+}
