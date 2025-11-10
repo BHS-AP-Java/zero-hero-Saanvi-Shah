4b09909d9134f47dc10a3e651e04065b29044783
Saanvi <163568541+1118540@users.noreply.github.com>
2025-11-10 02:49:15 +0000

    removing duplicate code

--- src/main/java/edu/bhscs/Table.java
+++ src/main/java/edu/bhscs/Table.java
@@ -20,7 +20,7 @@ public class Table {
     this.legStyle = "|"; // Default leg style
   }
 
-  // chANGE
+  // Change
   // Overloaded constructor with default height
   public Table(int legs, int width) {
     this(legs, width, 6); // Default height is 4
@@ -81,40 +81,4 @@ public class Table {
     System.out.println();
   }
 
-  {
-
-    // Draw the table legs
-    for (int row = 0; row < height; row++) {
-      for (int legNum = 0; legNum < legs; legNum++) {
-        int legPosition;
-        if (legs == 1) {
-          // One leg goes in the middle
-          legPosition = width / 2;
-        } else {
-          // Evenly space the legs
-          legPosition = legNum * (width - 1) / (legs - 1);
-        }
-
-        // Print spaces until we reach this leg's position
-        if (legNum == 0) {
-          // First leg: print spaces from the start
-          for (int space = 0; space < legPosition; space++) {
-            System.out.print(" ");
-          }
-        } else {
-          // Other legs: print spaces from the previous leg
-          int previousLegPosition = (legNum - 1) * (width - 1) / (legs - 1);
-          int gapSize = legPosition - previousLegPosition - 1;
-          for (int space = 0; space < gapSize; space++) {
-            System.out.print(" ");
-          }
-        }
-
-        // Draw the leg using the custom style
-        System.out.print(legStyle);
-      }
-      System.out.println();
-    }
-    System.out.println();
-  }
 }
